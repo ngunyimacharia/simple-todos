64151dfc17c281b9407fff94f27c2f4c26a54cf3
Ngunyi Macharia <ngunyimacharia@gmail.com>
2018-09-11 10:32:55 +0000

    Addition of API methods

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,17 +1,32 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
+
+import './task.js';
 import './body.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
 
 Template.body.helpers({
 
   tasks() {
+    const instance = Template.instance();
 
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 
-  // tasks: [
-  //   {text:'Hello'}
-  // ],
+  incompleteCount(){
+    return Tasks.find({checked: {$ne: true} }).count();
+  }
 
 });
 
@@ -23,12 +38,13 @@ Template.body.events({
    const target = event.target;
    const text = target.text.value;
    // Insert a task into the collection
-   Tasks.insert({
-     text,
-     createdAt: new Date(), // current time
-   });
+   Meteor.call('tasks.insert',text);
    // Clear form
    target.text.value = '';
  },
+
+ 'change .hide-completed input'(event, instance){
+   instance.state.set('hideCompleted',event.target.checked);
+ }
 
 });
---
+++ imports/ui/task.js
@@ -0,0 +1,18 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+import './task.html';
+Template.task.events({
+
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setChecked',this._id, !this.checked);
+  },
+
+  'click .delete'() {
+    var myId = this._id;
+    if(typeof myId == 'object'){
+      myId = this._id._str;
+    }
+    Meteor.call('tasks.remove',myId);
+  },
+});
